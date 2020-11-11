2dda27d58f2e6139a28dea97ec1448d84a33aa39
Hanif Abdullah <hanifabdullah7@gmail.com>
2020-11-11 14:50:58 +0700

    Add user interactivity

--- Badge3-AboutMe/app/src/main/java/com/hanifabdullah21/aboutme/MainActivity.kt
+++ Badge3-AboutMe/app/src/main/java/com/hanifabdullah21/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.hanifabdullah21.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addNickname(view: View) {
+        nickname_text.text = nickname_edit.text
+        nickname_text.visibility = View.VISIBLE
+        nickname_edit.visibility = View.GONE
+        done_button.visibility = View.GONE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun updateNickname(view: View) {
+        nickname_text.visibility = View.GONE
+        nickname_edit.visibility = View.VISIBLE
+        done_button.visibility = View.VISIBLE
+
+        nickname_edit.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(nickname_edit, 0)
     }
 }
