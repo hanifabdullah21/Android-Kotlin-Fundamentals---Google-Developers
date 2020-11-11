313f627b70ba432863c0ab3de184f3226ced0dee
Hanif Abdullah <hanifabdullah7@gmail.com>
2020-11-11 14:07:29 +0700

    Codelab Image resources and compatibility

--- Badge2-DiceRollerApp/app/src/main/java/com/hanifabdullah21/dicerollerapp/MainActivity.kt
+++ Badge2-DiceRollerApp/app/src/main/java/com/hanifabdullah21/dicerollerapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hanifabdullah21.dicerollerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.NumberFormatException
 
@@ -17,25 +16,57 @@ class MainActivity : AppCompatActivity() {
 
         //TODO Challenge Anatomy of Basic Android Project
         count_up.setOnClickListener {
-            try {
-                val number = count_up.text.toString().toInt()
-                if (number < 6){
-                    result_text.text = ""+(number+1)
-                }
-            }catch (e: NumberFormatException){
-                result_text.text = "1"
-            }
+            countUp()
         }
 
+        //TODO Homework Anatomy of Basic Android Project
         reset.setOnClickListener {
             result_text.text = "0"
+            result_text2.text = "0"
+            dice_image.setImageResource(getRandomDiceImage(0))
+            dice_image2.setImageResource(getRandomDiceImage(0))
         }
 
     }
 
+    private fun countUp() {
+        try {
+            val number = result_text.text.toString().toInt()
+            val number2 = result_text2.text.toString().toInt()
+            if (number < 6 && number2 < 6){
+                result_text.text = ""+(number+1)
+                result_text2.text = ""+(number2+1)
+                dice_image.setImageResource(getRandomDiceImage(number+1))
+                dice_image2.setImageResource(getRandomDiceImage(number2+1))
+            }
+        }catch (e: NumberFormatException){
+            result_text.text = "1"
+            result_text2.text = "1"
+            dice_image.setImageResource(getRandomDiceImage(1))
+            dice_image2.setImageResource(getRandomDiceImage(1))
+        }
+    }
+
     private fun rollDice() {
         val randomInt = (1..6).random()
+        val randomInt2 = (1..6).random()
         result_text.text = randomInt.toString()
-        Toast.makeText(this, "Roll Dice", Toast.LENGTH_SHORT).show()
+        result_text2.text = randomInt2.toString()
+
+        dice_image.setImageResource(getRandomDiceImage(randomInt))
+        dice_image2.setImageResource(getRandomDiceImage(randomInt2))
+    }
+
+    //TODO Challenge Image resources and compatibility
+    private fun getRandomDiceImage(randomInt: Int): Int{
+        return when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_empty
+        }
     }
 }
